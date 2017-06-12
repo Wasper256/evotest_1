da737c7217954776e23ff653f81f996743711fdd
wasper256 <andremorhun@gmail.com>
2017-06-13 00:27:08 +0300

    randomizer was added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Main app file."""
 from flask import Flask, render_template, request, flash
 from extentions import db
+from random import choice
 
 app = Flask(__name__)
 app.secret_key = 'i_want_in_evo_so_much'
@@ -12,20 +13,27 @@ from models import *
 @app.route('/', methods=['GET', 'POST'])
 def home():
     """Loading main page."""
-    if request.method == 'POST' and request.form['name']:
-        print("fsfsfs")
+    names = Names.query.all()
+    if request.method == 'POST' and "name" in request.form:
         try:  # simple getting data from forms
             name = request.form['name']
-            print("dsfsdfsdf")
             db.session.add(Names(name))
-            print("dsfsf")
             db.session.commit()
-            print("sfsdfsdf")
             flash("New name successfully added!")
         except:  # anticlone department logic
             flash("Error! Name already exists!")
-    else
+    elif request.method == 'POST' and "lucky" in request.form:
+        win = []
+        while len(win) < 3:
+            n = choice(names).name
+            if n in win:
+                print"1"
+                pass
+            else:
+                n = n.encode("utf-8")
+                win.append(n)
+        flash("Winners is: {0}, {1}, {2}.".format(win[0], win[1], win[2]))
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
